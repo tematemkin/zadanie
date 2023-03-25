0d2e40505094cbcdfd86bc1738caeb0f0d10014f
tematemkin <tema2612temkin@yandex.ru>
2023-03-25 08:45:11 +0300

    Операторы
    операторы присвоения

---
+++ main.js
@@ -0,0 +1,36 @@
+// операторы____________
+
+/* let num = 5; */
+
+console.log(2 + 3);
+
+console.log(5 - 2);
+console.log(9 * 9);
+console.log(10 / 5);
+console.log(10 % 2);
+console.log(10 % 3);  // 10/3=3 далее 3*3=9 далее 10-9=1
+
+// console.log(num - 3);
+
+/* --num;
+console.log(num); */
+
+/* console.log(--num);  // --num префиксный
+console.log(num); */
+
+/* console.log(num--);  // посфиксные
+console.log(num); */
+
+///+++++++++++++++Строки__________________________
+
+/* let srtring = 'Привет Мир';   
+
+console.log(srtring + ' Хорошая погода'); */
+
+//___________Булевые значения__________
+
+/* let num = 5;
+let string = '5';
+console.log(num === 5);
+console.log(num <= 3);
+console.log(num !== 4); */
